eaf783385d5fe29d8f4a1145a2e21eaf9849685d
Zenitsu-hacker <alieeee2026@gmail.com>
2024-07-26 20:19:13 +0330

    Add files via upload

---
+++ mixed.py
@@ -0,0 +1,62 @@
+from colorama import Fore,init
+init()
+from colorama import Fore,init
+init()
+print(Fore.BLUE+"  ")
+import time
+
+def printLow(Str):
+       
+       for char in Str:
+
+              print(char, end="", flush=True) 
+
+              time.sleep(0.0001)
+
+printLow(f"""
+@@&&&&@@@@@@@@@&&&&&&&&&&&&#######&&@#B@@@@@@@@@@@
+@@@@@@@@@@@@@&&######&##BBB###BB##BB#GG@@@@@@@@@@@
+@@&&&##&&@@&#####BBB#BGGBGGGGGBBGGB#GG&@@@@@@@@@@@
+@@@&&&&&&&###BBBBGGGGPPPBG55555PGP5PGGPB@@@&&@@@@@
+@&#&&@@&&#BBPPPBG555YYYY5BYYJYJJJ5YYYP5YPB&@&&&@@@
+@&&&&G5B#PGP555GYYYJJJJJJBP?J?????JJJJJJJJPG#&&&@@
+@&@@&GPPP55YYYY5YYJYJJJJ?5BY??????7?7????JPP#&&&&&
+&&&&&&BG5YYYYYY5PPY5Y????JGBYJPYJJJJ77777JYP#&&&&&
+#B!!!PP5JJJJY5PPGG5PP??7??PPP5PP555YJ?J??YYJG&&&&&
+##GGGBGPP5YYY5PPPP5YY55YYYPP??JY5JY5JYYYYYJP&&&&&&
+#&&&&&&BPPP5555?7Y5JJJY5YY55?JY5Y!~JYJYJYPB&&&&&&&
+&&&&&&&&PP555PY75G#&#B5YJ?J5G#&#GY~75JJJY5#&&&&&&&
+JP&&&&&&&#BGPGY!7!?J!~~~^::^^~7!~~^J5YG#&&&&&&&&&&
+^!#&&&&&&&&&5JJ~^^^:::.^!:..::::::^7?JG#&&&&&&&&&&
+B#&&&&&&&&&BP57!^......:^........^7?YY5B&&&&&&&&&&
+&&&&&&&&&&&&#BP5Y~:......:.:...:~J5PB##&&&&&&&&&&&
+&&&&&&&&&&&&####BPY7^:..^!:..^!JPGB#&&&&&&&&&&&&&&
+&&&&&&&&&&&&#GP55PPP5Y?~^:^7Y5PPGPJ?7J5YYPGBB#&&&&
+&&&&&&&#BGY7~:^!75&#BBGP5Y5PBB#&&&?!~~~^~~!!!7?YB&
+&&&&BPY?!!~~~~~~~J@&&&@G5PPG@@&&&@B!!~~~~~~!!!!!7J
+&&#5?7!!~~~~~~~~!?&&&&&&BPG&&&&@&&&5!!!!!!!!!!!!!7
+&GJ77!!!~~~~~~~~!7#&&&&@BGG&@@&&&&@&?!!!!!!!!!!!:^
+5??!~!!!!!!!:.~!!7G&&&&&@@@@&&&&&&&@G77!.^!7!!7!^^
+J?!:.!!!!7?: ..!!7P&&&&&&&&&&&&&&####J7!^^!J?7777?
+
+[virus]-link1
+[Algorithm]-link2
+[Translation]-link3
+[D script]-link4
+[sex upload]-link5
+[Sex the video upload]-link6
+[Destroyer of Basij]-link7
+[terrorist link]-link8
+[Rubika hack site]-link9
+[dark web]-link10
+[Death to Khamenei]-link11
+[bug]-link12
+""")
+print(Fore.LIGHTRED_EX+"  ")
+num_links = 12
+links = [input(f"Enter link {i+1}: ") for i in range(num_links)]
+mixed_text = '#This user threatens the people of Rubika to hack filtering)(@Supportbot)=(@admin_rubika)=[{}]><({})/>({})bing;rubika:( bn : (5.106.8.151)time:(5.106.8.151))({})><({})><({})["{}"]=["{}"]=["{}"]=["{}"]=["{}"]+["{}"Report.py("report-account-sexual-xXx-in-yftt15k-rubika.py")'.format(*links)
+print(Fore.YELLOW+"")
+print("↓↓↓")
+print(Fore.GREEN+"  ")
+print(mixed_text)
